681ac427dc49fe50d7033e15b5acb6a29673e8f5
Jhinzu <jeandetorres30140@gmail.com>
2023-01-17 16:29:06 +0100

    fin

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const mi = document.getElementById('mi');
 const player = document.getElementById('player');
 const ia = document.getElementById('ia');
 const command = document.getElementById('command');
+const tryAgainElt = document.getElementById("restart");
+const scoreElt = document.querySelector(".score");
 
+console.log (scoreElt);
 shi.addEventListener('click', () => play('shi'));
 
 fu.addEventListener('click', () => play('fu'));
@@ -31,6 +34,104 @@ function play(userChoice)
 
     player.src = "img/"+userChoice+".png";
     player.classList.add("translate-left");
+
+    let scorePlayerElt = document.querySelector("#score-player");
+    let scoreIaElt = document.querySelector("#score-ia");
+    const h3Add = document.createElement("h3");
+
+    //gagner
+    if (userChoice == "shi" && iaChoice == "fu" || userChoice == "fu" && iaChoice == "mi" || userChoice == "mi" && iaChoice == "shi" )
+    {
+        console.log("gagner");
+        scorePlayerElt.innerHTML++;
+
+        //fin de partie si gagner 
+        if(scorePlayerElt.innerHTML == 3)
+        {
+            command.className= "hidden";
+            tryAgainElt.className = "btn-restart";
+            scoreElt.insertAdjacentElement("afterend",h3Add);
+            h3Add.append("win !");
+
+            //reset avec le bouton
+            tryAgainElt.addEventListener("click",()=>
+            {
+                scoreIaElt.innerHTML = "0" ;
+                scorePlayerElt.innerHTML = "0";
+                command.className = "show";
+                tryAgainElt.className = "hidden";
+                h3Add.remove();
+            })
+        }
+        
+    }
+    //perdu
+    else if (userChoice == "shi" && iaChoice == "mi" || userChoice == "fu" && iaChoice == "shi" || userChoice == "mi" && iaChoice == "fu")
+    {
+        console.log("perdu");
+        scoreIaElt.innerHTML++;
+
+        //fin de partie si perdu
+        if (scoreIaElt.innerHTML == 3)
+        {
+            command.className= "hidden";
+            tryAgainElt.className = "btn-restart";
+            scoreElt.insertAdjacentElement("afterend",h3Add);
+            h3Add.append("Loose !");
+            
+            tryAgainElt.addEventListener("click",()=>
+            {
+                scoreIaElt.innerHTML = "0" ;
+                scorePlayerElt.innerHTML = "0";
+                command.classList.add("show");
+                tryAgainElt.className = "hidden";
+                h3Add.remove();
+
+            })
+        }
+    }
+    //égaliter
+    else if (userChoice == iaChoice)
+    {
+        scorePlayerElt.innerHTML++;
+        scoreIaElt.innerHTML++;
+
+        //fin de partie si égaliter joueurs et player
+        if(scorePlayerElt.innerHTML == 3)
+        {
+            command.className= "hidden";
+            tryAgainElt.className = "btn-restart";
+            scoreElt.insertAdjacentElement("afterend",h3Add);
+            h3Add.append("Match null !");
+            tryAgainElt.addEventListener("click",()=>
+            {
+                scoreIaElt.innerHTML = "0" ;
+                scorePlayerElt.innerHTML = "0";
+                command.classList.add("show");
+                tryAgainElt.className = "hidden";
+                h3Add.className = "hidden";
+                h3Add.remove();
+
+            })
+
+        }
+        else if (scoreIaElt.innerHTML == 3)
+        {
+            command.className= "hidden";
+            tryAgainElt.className = "btn-restart";
+            scoreElt.insertAdjacentElement("afterend",h3Add);
+            h3Add.append("Match null !");
+            tryAgainElt.addEventListener("click",()=>
+            {
+                scoreIaElt.innerHTML = "0" ;
+                scorePlayerElt.innerHTML = "0";
+                command.classList.add("show");
+                h3Add.className = "hidden";
+                h3Add.remove();
+            })
+        }
+    }
+    
 }
 
 function reset_animation()
